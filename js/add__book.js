a71fb1d0bb4d6279afc3c0fd37915a104ab10d45
Konstantin Tanasenko <tanasenkok@gmail.com>
2022-01-12 16:19:14 +0400

    Исправил отправку формы

--- js/add__book.js
+++ js/add__book.js
@@ -58,6 +58,7 @@ function giveBook() {
     let titleBook = document.createElement('input');
     titleBook.classList.add('title__book');
     titleBook.setAttribute('type', 'text');
+    titleBook.setAttribute('name', 'login');
     titleBook.setAttribute('placeholder', 'Заголовок');
     formContent.append(titleBook);
 
@@ -68,40 +69,48 @@ function giveBook() {
     let selectBook = document.createElement('input');
     selectBook.classList.add('select__book');
     selectBook.setAttribute('type', 'file');
+    selectBook.setAttribute('name', 'file');
     selectBook.setAttribute('accept', '.txt, .doc, .docx');
     container.append(selectBook);
 
-    let addBook = document.createElement('button');
+    let addBook = document.createElement('input');
     addBook.classList.add('add__book');
-    addBook.innerText = 'Загрузить';
+    addBook.setAttribute('type', 'submit');
+    addBook.setAttribute('name', 'submit');
+    addBook.setAttribute('value', 'Загрузить');
     container.append(addBook);
 
     form.append(formContent);
 
-    addBook.addEventListener('click', async(e)=>{
-        e.preventDefault();
-        let book = {};
-        book.login = titleBook.value;
-        let file = selectBook.files[0];
-        let reader = new FileReader();
-        reader.readAsText(file);
-        reader.onload = function() {
-            book.file = reader.result;
-        };
+    const url = 'https://apiinterns.osora.ru'
 
-        let response = await fetch('https://apiinterns.osora.ru', {
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault()
+
+        const file = document.querySelector('[type=file]').files
+        const formData = new FormData()
+
+        formData.append('login', titleBook.value)
+
+        for (let i = 0; i < file.length; i++) {
+            let fil = file[i]
+
+            formData.append('file', fil)
+        }        
+
+        fetch(url, {
             method: 'POST',
-            headers: {
-                'Content-Type': 'text/plain;charset=UTF-8'
-            },
-            body: JSON.stringify(book)
-        });
+            body: formData,
+        }).then((response) => {
+            console.log(response)
+        }) 
 
-        let result = await response.json();
-        console.log(result.message);
-        
-        localStorage.setItem(book.login, book.file);
+        let dataArr = Array.from(formData.entries());
+
+        let key = dataArr[0];
+        let value = dataArr[1];
+
+        localStorage.setItem(key[1], value[1]);
         console.log(localStorage);
     })
 }
-
